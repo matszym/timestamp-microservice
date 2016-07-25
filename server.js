9e6e3f84a0fb3747ee5486ea330c424b7dc5b5f0
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-25 17:47:13 +0200

    add route to server

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 let express = require('express'),
 app = express(),
 config = require('./config/config.js'),
+timeCtrl = require('./controllers/time.server.controller.js'),
 server;
+
+app.use('/:time', timeCtrl.getTime);
 
 server = app.listen(config.port);
 console.log('server is listening on port', config.port);
